e8340984c12ec25ed52ef699417887915474c9fc
shindo-cnt <shindo@concentinc.jp>
2017-07-18 00:34:22 +0900

    Security update - gw.php - avoid non-ajax access, avoid non-web url request

--- gw.php
+++ gw.php
@@ -5,6 +5,16 @@
 $confCommon = new ConfCommon;
 $url = preg_replace('/^(.*\/\/)(.*?)$/', '${1}' . '${2}', htmlspecialchars($_GET['url'], ENT_QUOTES, 'UTF-8'));
 
+if((! isset($_SERVER['HTTP_X_REQUESTED_WITH'])) || (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')) {
+//	error_log('[' . $_SERVER['SCRIPT_NAME'] . ']: ' . 'Non-Ajax Access. Operation Stopeed');
+	exit(1);
+}
+
+if(!preg_match('/(?:^|[\s　]+)((?:https?|ftp):\/\/[^\s　]+)/', $url)) {
+//	error_log('invalid format in URL - ' . $url);
+	exit(0);
+}
+
 $listConfHttpRequest = array();
 if(!empty($confCommon -> listConf['authId']) && !empty($confCommon -> listConf['authPass'])) {
 	$listConfHttpRequest['header'] = "Authorization: Basic " . base64_encode($confCommon -> listConf['authId']. ':' . $confCommon -> listConf['authPass']);
