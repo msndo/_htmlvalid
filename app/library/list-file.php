9b4efc98e63d3d92d846f743364655e0778e9a1f
shindo-cnt <shindo@concentinc.jp>
2017-07-19 00:44:44 +0900

    コード書式修正

--- app/library/list-file.php
+++ app/library/list-file.php
@@ -3,47 +3,56 @@ class ListFile {
 
 	// 配下全階層にあるファイルの入れ子リスト
 	function getFileListRecursive($dir) {
-    	$files = glob(rtrim($dir, '/') . '/*');
-    	$list = array();
-    	foreach ($files as $file) {
-        	if (is_file($file) && preg_match('/\.html$/', $file)) {
-            	$list[$file] = $file;
-        	}
-        	if (is_dir($file)) {
-            	$list[$file . '/']  = $this -> getFileListRecursive($file);
-        	}
-    	}
+		$files = glob(rtrim($dir, '/') . '/*');
+		$list = array();
+		foreach ($files as $file) {
+			if (is_file($file) && preg_match('/\.html$/', $file)) {
+					$list[$file] = $file;
+			}
+			if (is_dir($file)) {
+					$list[$file . '/'] = $this -> getFileListRecursive($file);
+			}
+		}
 
-    	return $list;
+		return $list;
 	}
 
 
 	// 配下全階層にあるファイルの直列リスト
-    // 負荷注意
+	// 負荷注意
 	function getFileListLinear($dir) {
-    	$files = glob(rtrim($dir, '/') . '/*');
-    	$list = array();
-    	foreach ($files as $file) {
-        	if (is_file($file)) {
-            	$list[] = $file;
-        	}
-        	if (is_dir($file)) {
-            	$list = array_merge($list, $this -> getFileListLinear($file));
-        	}
-    	}
+		$files = glob(rtrim($dir, '/') . '/*');
+		$list = array();
+		foreach ($files as $file) {
+			if (is_file($file)) {
+					$list[] = $file;
+			}
+			if (is_dir($file)) {
+					$list = array_merge($list, $this -> getFileListLinear($file));
+			}
+		}
 
-    	return $list;
+		return $list;
 	}
 
 	// 直下階層分、単一レベルのファイルリストを返す
 	function getFileListSingleLevel($dir) {
-    	$files = glob(rtrim($dir, '/') . '/*');
-    	$list = array();
-    	foreach ($files as $file) {
-            $list[] = $file;
-    	}
+		$files = glob(rtrim($dir, '/') . '/*');
+		$list = array();
+		foreach ($files as $file) {
+					$list[] = $file;
+		}
 
-    	return $list;
+		return $list;
+	}
+
+	// ディレクトリ正当性チェック
+	function isPathUnderDocRoot($path, $siteRoot) {
+		if(!preg_match('/^' . str_replace('/', '\/', $siteRoot) . '/', realpath($path) . '/')) {
+				return false;
+		}
+
+		return true;
 	}
 }
 ?>
